53cf5728b44a9ef4ad91ab9a3b6ca90de3817a3a
Ryan Mohr <ryan.mohr@gmail.com>
2015-10-01 08:24:26 -1000

    Fix tooltip initialization

--- docs.js
+++ docs.js
@@ -1,1 +1,16 @@
-$("[title]").tooltip();
+require(["gitbook"], function(gitbook) {
+  gitbook.events.bind("start", function(e, config) {
+    init();
+  });
+
+  gitbook.events.bind("page.change", function() {
+    init();
+  });
+});
+
+// The start event never fires so we just call init ourselves on dom ready.
+$(init);
+
+function init() {
+  $("[title]").tooltip();
+}
